f5f0dda177571f2c5602f7f24431c0affdfc8647
Loui <25903926+loui1024@users.noreply.github.com>
2023-11-11 19:02:19 +0000

    Shadow Bias Correction

--- src/engine/scripts/File.cpp
+++ src/engine/scripts/File.cpp
@@ -83,8 +83,8 @@ namespace LouiEriksson {
 					stbi_image_free(data);
 					
 					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 					
 					if (_generateMipmaps) {
 						glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
