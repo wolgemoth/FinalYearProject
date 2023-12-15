84f2a2d939929f6baf4e4d7c664eb904bca8c427
wolgemoth <25903926+loui1024@users.noreply.github.com>
2023-12-15 00:52:18 +0000

    Add Comments to Utils.cpp

--- src/engine/scripts/Utils.cpp
+++ src/engine/scripts/Utils.cpp
@@ -4,13 +4,19 @@
 
 namespace LouiEriksson {
 	
+	/// <summary>
+	/// Split a string in a collection of substrings using a divider.
+	/// </summary>
 	std::vector<std::string> Utils::Split(const std::string& _string, const char& _divider) {
 		
 		std::vector<std::string> result;
 		
+		// Init string stream.
 		std::istringstream iss(_string);
 		
 		std::string substring;
+		
+		// Split string stream using divider and push into result.
 		while (std::getline(iss, substring, _divider)) {
 			result.push_back(substring);
 		}
@@ -19,16 +25,22 @@ namespace LouiEriksson {
 	}
 	
 	/// <summary>
-	/// Trim leading and trailing whitespace characters from a c++ string.
+	/// Trim leading and trailing whitespace characters from a string.
 	/// </summary>
 	std::string Utils::Trim(const std::string& _string) {
 		
+		// Use a regex pattern to trim the leading and trailing whitespace characters from the string.
 		std::smatch sm;
 		return std::regex_search(_string, sm, std::regex(R"(^(\S)(.+))")) ?
 			sm[0] : _string;
 	}
 	
+	/// <summary>
+	/// Remap a float from one range to another.
+	/// </summary>
 	float Utils::Remap(const float& _val, const float& _aMin, const float& _aMax, const float& _bMin, const float& _bMax) {
+		
+		// Remap function I made for a homework task in "Mathematics for Computer Graphics".
 		return _bMin + (_val - _aMin) * (_bMax - _bMin) / (_aMax - _aMin);
 	}
 	
