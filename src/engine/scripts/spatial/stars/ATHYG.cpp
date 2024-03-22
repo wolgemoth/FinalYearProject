429e950ccda35dc0ee57a0239150fab3941d482e
wolgemoth <25903926+loui1024@users.noreply.github.com>
2024-03-22 23:20:58 +0000

    Better Deserialisation of ATHYG Data

--- src/engine/scripts/spatial/stars/ATHYG.cpp
+++ src/engine/scripts/spatial/stars/ATHYG.cpp
@@ -1,5 +1,7 @@
 #include "ATHYG.h"
 
+#include "../../core/utils/Utils.h"
+
 #include <string>
 #include <array>
 
@@ -7,104 +9,104 @@ namespace LouiEriksson::Engine::Spatial {
 	
 	ATHYG::V1::V1(const std::array<std::string, 23>& _values) {
 		
-		try { id        = std::stoul(_values[ 0]); } catch(...) { id        = {}; }
-		try { tyc       =            _values[ 1] ; } catch(...) { tyc       = {}; }
-		try { gaia      = std::stoul(_values[ 2]); } catch(...) { gaia      = {}; }
-		try { hyg       = std::stoul(_values[ 3]); } catch(...) { hyg       = {}; }
-		try { hip       = std::stoul(_values[ 4]); } catch(...) { hip       = {}; }
-		try { hd        = std::stoul(_values[ 5]); } catch(...) { hd        = {}; }
-		try { hr        = std::stoul(_values[ 6]); } catch(...) { hr        = {}; }
-		try { gl        =            _values[ 7] ; } catch(...) { gl        = {}; }
-		try { bayer     =            _values[ 8] ; } catch(...) { bayer     = {}; }
-		try { flam      =            _values[ 9] ; } catch(...) { flam      = {}; }
-		try { con       =            _values[10] ; } catch(...) { con       = {}; }
-		try { proper    =            _values[11] ; } catch(...) { proper    = {}; }
-		try { ra        = std::stof(_values[12]) ; } catch(...) { ra        = {}; }
-		try { dec       = std::stof(_values[13]) ; } catch(...) { dec       = {}; }
-		try { pos_src   =           _values[14]  ; } catch(...) { pos_src   = {}; }
-		try { dist      = std::stof(_values[15]) ; } catch(...) { dist      = {}; }
-		try { x0        = std::stof(_values[16]) ; } catch(...) { x0        = {}; }
-		try { y0        = std::stof(_values[17]) ; } catch(...) { y0        = {}; }
-		try { z0        = std::stof(_values[18]) ; } catch(...) { z0        = {}; }
-		try { dist_src  =           _values[19]  ; } catch(...) { dist_src  = {}; }
-		try { mag       = std::stof(_values[20]) ; } catch(...) { mag       = {}; }
-		try { absmag    = std::stof(_values[21]) ; } catch(...) { absmag    = {}; }
-		try { mag_src   =           _values[22]  ; } catch(...) { mag_src   = {}; }
+		Utils::TryParse<unsigned long>(_values[ 0], id      );
+		Utils::TryParse<std::string  >(_values[ 1], tyc     );
+		Utils::TryParse<unsigned long>(_values[ 2], gaia    );
+		Utils::TryParse<unsigned long>(_values[ 3], hyg     );
+		Utils::TryParse<unsigned long>(_values[ 4], hip     );
+		Utils::TryParse<unsigned long>(_values[ 5], hd      );
+		Utils::TryParse<unsigned long>(_values[ 6], hr      );
+		Utils::TryParse<std::string  >(_values[ 7], gl      );
+		Utils::TryParse<std::string  >(_values[ 8], bayer   );
+		Utils::TryParse<std::string  >(_values[ 9], flam    );
+		Utils::TryParse<std::string  >(_values[10], con     );
+		Utils::TryParse<std::string  >(_values[11], proper  );
+		Utils::TryParse<double       >(_values[12], ra      );
+		Utils::TryParse<double       >(_values[13], dec     );
+		Utils::TryParse<std::string  >(_values[14], pos_src );
+		Utils::TryParse<double       >(_values[15], dist    );
+		Utils::TryParse<double       >(_values[16], x0      );
+		Utils::TryParse<double       >(_values[17], y0      );
+		Utils::TryParse<double       >(_values[18], z0      );
+		Utils::TryParse<std::string  >(_values[19], dist_src);
+		Utils::TryParse<double       >(_values[20], mag     );
+		Utils::TryParse<double       >(_values[21], absmag  );
+		Utils::TryParse<std::string  >(_values[22], mag_src );
 	}
 	
 	ATHYG::V2::V2(const std::array<std::string, 33>& _values) {
 		
-		try { id        = std::stoul(_values[ 0]); } catch(...) { id        = {}; }
-		try { tyc       =            _values[ 1] ; } catch(...) { tyc       = {}; }
-		try { gaia      = std::stoul(_values[ 2]); } catch(...) { gaia      = {}; }
-		try { hyg       = std::stoul(_values[ 3]); } catch(...) { hyg       = {}; }
-		try { hip       = std::stoul(_values[ 4]); } catch(...) { hip       = {}; }
-		try { hd        = std::stoul(_values[ 5]); } catch(...) { hd        = {}; }
-		try { hr        = std::stoul(_values[ 6]); } catch(...) { hr        = {}; }
-		try { gl        =            _values[ 7] ; } catch(...) { gl        = {}; }
-		try { bayer     =            _values[ 8] ; } catch(...) { bayer     = {}; }
-		try { flam      =            _values[ 9] ; } catch(...) { flam      = {}; }
-		try { con       =            _values[10] ; } catch(...) { con       = {}; }
-		try { proper    =            _values[11] ; } catch(...) { proper    = {}; }
-		try { ra        = std::stof(_values[12]) ; } catch(...) { ra        = {}; }
-		try { dec       = std::stof(_values[13]) ; } catch(...) { dec       = {}; }
-		try { pos_src   =           _values[14]  ; } catch(...) { pos_src   = {}; }
-		try { dist      = std::stof(_values[15]) ; } catch(...) { dist      = {}; }
-		try { x0        = std::stof(_values[16]) ; } catch(...) { x0        = {}; }
-		try { y0        = std::stof(_values[17]) ; } catch(...) { y0        = {}; }
-		try { z0        = std::stof(_values[18]) ; } catch(...) { z0        = {}; }
-		try { dist_src  =           _values[19]  ; } catch(...) { dist_src  = {}; }
-		try { mag       = std::stof(_values[20]) ; } catch(...) { mag       = {}; }
-		try { absmag    = std::stof(_values[21]) ; } catch(...) { absmag    = {}; }
-		try { mag_src   =           _values[22]  ; } catch(...) { mag_src   = {}; }
-		try { rv        = std::stof(_values[23]) ; } catch(...) { rv        = {}; }
-		try { rv_src    =           _values[24]  ; } catch(...) { rv_src    = {}; }
-		try { pm_ra     = std::stof(_values[25]) ; } catch(...) { pm_ra     = {}; }
-		try { pm_dec    = std::stof(_values[26]) ; } catch(...) { pm_dec    = {}; }
-		try { pm_src    = std::stof(_values[27]) ; } catch(...) { pm_src    = {}; }
-		try { vx        = std::stof(_values[28]) ; } catch(...) { vx        = {}; }
-		try { vy        = std::stof(_values[29]) ; } catch(...) { vy        = {}; }
-		try { vz        = std::stof(_values[30]) ; } catch(...) { vz        = {}; }
-		try { spect     = std::stof(_values[31]) ; } catch(...) { spect     = {}; }
-		try { spect_src =           _values[32]  ; } catch(...) { spect_src = {}; }
+		Utils::TryParse<unsigned long>(_values[ 0], id       );
+		Utils::TryParse<std::string  >(_values[ 1], tyc      );
+		Utils::TryParse<unsigned long>(_values[ 2], gaia     );
+		Utils::TryParse<unsigned long>(_values[ 3], hyg      );
+		Utils::TryParse<unsigned long>(_values[ 4], hip      );
+		Utils::TryParse<unsigned long>(_values[ 5], hd       );
+		Utils::TryParse<unsigned long>(_values[ 6], hr       );
+		Utils::TryParse<std::string  >(_values[ 7], gl       );
+		Utils::TryParse<std::string  >(_values[ 8], bayer    );
+		Utils::TryParse<std::string  >(_values[ 9], flam     );
+		Utils::TryParse<std::string  >(_values[10], con      );
+		Utils::TryParse<std::string  >(_values[11], proper   );
+		Utils::TryParse<double       >(_values[12], ra       );
+		Utils::TryParse<double       >(_values[13], dec      );
+		Utils::TryParse<std::string  >(_values[14], pos_src  );
+		Utils::TryParse<double       >(_values[15], dist     );
+		Utils::TryParse<double       >(_values[16], x0       );
+		Utils::TryParse<double       >(_values[17], y0       );
+		Utils::TryParse<double       >(_values[18], z0       );
+		Utils::TryParse<std::string  >(_values[19], dist_src );
+		Utils::TryParse<double       >(_values[20], mag      );
+		Utils::TryParse<double       >(_values[21], absmag   );
+		Utils::TryParse<std::string  >(_values[22], mag_src  );
+		Utils::TryParse<double       >(_values[23], rv       );
+		Utils::TryParse<std::string  >(_values[24], rv_src   );
+		Utils::TryParse<double       >(_values[25], pm_ra    );
+		Utils::TryParse<double       >(_values[26], pm_dec   );
+		Utils::TryParse<double       >(_values[27], pm_src   );
+		Utils::TryParse<double       >(_values[28], vx       );
+		Utils::TryParse<double       >(_values[29], vy       );
+		Utils::TryParse<double       >(_values[30], vz       );
+		Utils::TryParse<double       >(_values[31], spect    );
+		Utils::TryParse<std::string  >(_values[32], spect_src);
 	}
 	
 	ATHYG::V3::V3(const std::array<std::string, 34>& _values) {
 
-		try { id        = std::stoul(_values[ 0]); } catch(...) { id        = {}; }
-		try { tyc       =            _values[ 1] ; } catch(...) { tyc       = {}; }
-		try { gaia      = std::stoul(_values[ 2]); } catch(...) { gaia      = {}; }
-		try { hyg       = std::stoul(_values[ 3]); } catch(...) { hyg       = {}; }
-		try { hip       = std::stoul(_values[ 4]); } catch(...) { hip       = {}; }
-		try { hd        = std::stoul(_values[ 5]); } catch(...) { hd        = {}; }
-		try { hr        = std::stoul(_values[ 6]); } catch(...) { hr        = {}; }
-		try { gl        =            _values[ 7] ; } catch(...) { gl        = {}; }
-		try { bayer     =            _values[ 8] ; } catch(...) { bayer     = {}; }
-		try { flam      =            _values[ 9] ; } catch(...) { flam      = {}; }
-		try { con       =            _values[10] ; } catch(...) { con       = {}; }
-		try { proper    =            _values[11] ; } catch(...) { proper    = {}; }
-		try { ra        = std::stof(_values[12]) ; } catch(...) { ra        = {}; }
-		try { dec       = std::stof(_values[13]) ; } catch(...) { dec       = {}; }
-		try { pos_src   =           _values[14]  ; } catch(...) { pos_src   = {}; }
-		try { dist      = std::stof(_values[15]) ; } catch(...) { dist      = {}; }
-		try { x0        = std::stof(_values[16]) ; } catch(...) { x0        = {}; }
-		try { y0        = std::stof(_values[17]) ; } catch(...) { y0        = {}; }
-		try { z0        = std::stof(_values[18]) ; } catch(...) { z0        = {}; }
-		try { dist_src  =           _values[19]  ; } catch(...) { dist_src  = {}; }
-		try { mag       = std::stof(_values[20]) ; } catch(...) { mag       = {}; }
-		try { absmag    = std::stof(_values[21]) ; } catch(...) { absmag    = {}; }
-		try { ci        = std::stof(_values[22]) ; } catch(...) { ci        = {}; }
-		try { mag_src   =           _values[23]  ; } catch(...) { mag_src   = {}; }
-		try { rv        = std::stof(_values[24]) ; } catch(...) { rv        = {}; }
-		try { rv_src    =           _values[25]  ; } catch(...) { rv_src    = {}; }
-		try { pm_ra     = std::stof(_values[26]) ; } catch(...) { pm_ra     = {}; }
-		try { pm_dec    = std::stof(_values[27]) ; } catch(...) { pm_dec    = {}; }
-		try { pm_src    = std::stof(_values[28]) ; } catch(...) { pm_src    = {}; }
-		try { vx        = std::stof(_values[29]) ; } catch(...) { vx        = {}; }
-		try { vy        = std::stof(_values[30]) ; } catch(...) { vy        = {}; }
-		try { vz        = std::stof(_values[31]) ; } catch(...) { vz        = {}; }
-		try { spect     = std::stof(_values[32]) ; } catch(...) { spect     = {}; }
-		try { spect_src =           _values[33]  ; } catch(...) { spect_src = {}; }
+		Utils::TryParse<unsigned long>(_values[ 0], id       );
+		Utils::TryParse<std::string  >(_values[ 1], tyc      );
+		Utils::TryParse<unsigned long>(_values[ 2], gaia     );
+		Utils::TryParse<unsigned long>(_values[ 3], hyg      );
+		Utils::TryParse<unsigned long>(_values[ 4], hip      );
+		Utils::TryParse<unsigned long>(_values[ 5], hd       );
+		Utils::TryParse<unsigned long>(_values[ 6], hr       );
+		Utils::TryParse<std::string  >(_values[ 7], gl       );
+		Utils::TryParse<std::string  >(_values[ 8], bayer    );
+		Utils::TryParse<std::string  >(_values[ 9], flam     );
+		Utils::TryParse<std::string  >(_values[10], con      );
+		Utils::TryParse<std::string  >(_values[11], proper   );
+		Utils::TryParse<double       >(_values[12], ra       );
+		Utils::TryParse<double       >(_values[13], dec      );
+		Utils::TryParse<std::string  >(_values[14], pos_src  );
+		Utils::TryParse<double       >(_values[15], dist     );
+		Utils::TryParse<double       >(_values[16], x0       );
+		Utils::TryParse<double       >(_values[17], y0       );
+		Utils::TryParse<double       >(_values[18], z0       );
+		Utils::TryParse<std::string  >(_values[19], dist_src );
+		Utils::TryParse<double       >(_values[20], mag      );
+		Utils::TryParse<double       >(_values[21], absmag   );
+		Utils::TryParse<double       >(_values[22], ci       );
+		Utils::TryParse<std::string  >(_values[23], mag_src  );
+		Utils::TryParse<double       >(_values[24], rv       );
+		Utils::TryParse<std::string  >(_values[25], rv_src   );
+		Utils::TryParse<double       >(_values[26], pm_ra    );
+		Utils::TryParse<double       >(_values[27], pm_dec   );
+		Utils::TryParse<double       >(_values[28], pm_src   );
+		Utils::TryParse<double       >(_values[29], vx       );
+		Utils::TryParse<double       >(_values[30], vy       );
+		Utils::TryParse<double       >(_values[31], vz       );
+		Utils::TryParse<double       >(_values[32], spect    );
+		Utils::TryParse<std::string  >(_values[33], spect_src);
     }
 	
 } // LouiEriksson::Engine::Spatial
--- src/engine/scripts/spatial/stars/ATHYG.h
+++ src/engine/scripts/spatial/stars/ATHYG.h
@@ -8,112 +8,112 @@ namespace LouiEriksson::Engine::Spatial {
 
 	struct ATHYG {
 		
-		struct [[maybe_unused]] V1 {
+		struct V1 {
 		
-			[[maybe_unused]] unsigned long id;
-			[[maybe_unused]] std::string   tyc;
-			[[maybe_unused]] unsigned long gaia;
-			[[maybe_unused]] unsigned long hyg;
-			[[maybe_unused]] unsigned long hip;
-			[[maybe_unused]] unsigned long hd;
-			[[maybe_unused]] unsigned long hr;
-			[[maybe_unused]] std::string   gl;
-			[[maybe_unused]] std::string   bayer;
-			[[maybe_unused]] std::string   flam;
-			[[maybe_unused]] std::string   con;
-			[[maybe_unused]] std::string   proper;
-			[[maybe_unused]] double        ra;
-			[[maybe_unused]] double        dec;
-			[[maybe_unused]] std::string   pos_src;
-			[[maybe_unused]] double        dist;
-			[[maybe_unused]] double        x0;
-			[[maybe_unused]] double        y0;
-			[[maybe_unused]] double        z0;
-			[[maybe_unused]] std::string   dist_src;
-			[[maybe_unused]] double        mag;
-			[[maybe_unused]] double        absmag;
-			[[maybe_unused]] std::string   mag_src;
-	    
-            [[maybe_unused]] explicit V1(const std::array<std::string, 23>& _values);
+			  unsigned long id;
+			  std::string   tyc;
+			  unsigned long gaia;
+			  unsigned long hyg;
+			  unsigned long hip;
+			  unsigned long hd;
+			  unsigned long hr;
+			  std::string   gl;
+			  std::string   bayer;
+			  std::string   flam;
+			  std::string   con;
+			  std::string   proper;
+			  double        ra;
+			  double        dec;
+			  std::string   pos_src;
+			  double        dist;
+			  double        x0;
+			  double        y0;
+			  double        z0;
+			  std::string   dist_src;
+			  double        mag;
+			  double        absmag;
+			  std::string   mag_src;
+			  
+			  explicit V1(const std::array<std::string, 23>& _values);
 		};
 		
-		struct [[maybe_unused]] V2 {
+		struct V2 {
 		
-			[[maybe_unused]] unsigned long id;
-			[[maybe_unused]] std::string   tyc;
-			[[maybe_unused]] unsigned long gaia;
-			[[maybe_unused]] unsigned long hyg;
-			[[maybe_unused]] unsigned long hip;
-			[[maybe_unused]] unsigned long hd;
-			[[maybe_unused]] unsigned long hr;
-			[[maybe_unused]] std::string   gl;
-			[[maybe_unused]] std::string   bayer;
-			[[maybe_unused]] std::string   flam;
-			[[maybe_unused]] std::string   con;
-			[[maybe_unused]] std::string   proper;
-			[[maybe_unused]] double        ra;
-			[[maybe_unused]] double        dec;
-			[[maybe_unused]] std::string   pos_src;
-			[[maybe_unused]] double        dist;
-			[[maybe_unused]] double        x0;
-			[[maybe_unused]] double        y0;
-			[[maybe_unused]] double        z0;
-			[[maybe_unused]] std::string   dist_src;
-			[[maybe_unused]] double        mag;
-			[[maybe_unused]] double        absmag;
-			[[maybe_unused]] std::string   mag_src;
-			[[maybe_unused]] double        rv;
-			[[maybe_unused]] std::string   rv_src;
-			[[maybe_unused]] double        pm_ra;
-			[[maybe_unused]] double        pm_dec;
-			[[maybe_unused]] double        pm_src;
-			[[maybe_unused]] double        vx;
-			[[maybe_unused]] double        vy;
-			[[maybe_unused]] double        vz;
-			[[maybe_unused]] double        spect;
-			[[maybe_unused]] std::string   spect_src;
-	    
-            [[maybe_unused]] explicit V2(const std::array<std::string, 33>& _values);
+			  unsigned long id;
+			  std::string   tyc;
+			  unsigned long gaia;
+			  unsigned long hyg;
+			  unsigned long hip;
+			  unsigned long hd;
+			  unsigned long hr;
+			  std::string   gl;
+			  std::string   bayer;
+			  std::string   flam;
+			  std::string   con;
+			  std::string   proper;
+			  double        ra;
+			  double        dec;
+			  std::string   pos_src;
+			  double        dist;
+			  double        x0;
+			  double        y0;
+			  double        z0;
+			  std::string   dist_src;
+			  double        mag;
+			  double        absmag;
+			  std::string   mag_src;
+			  double        rv;
+			  std::string   rv_src;
+			  double        pm_ra;
+			  double        pm_dec;
+			  double        pm_src;
+			  double        vx;
+			  double        vy;
+			  double        vz;
+			  double        spect;
+			  std::string   spect_src;
+			  
+			  explicit V2(const std::array<std::string, 33>& _values);
 		};
 		
-		struct [[maybe_unused]] V3 {
+		struct V3 {
 		
-			[[maybe_unused]] unsigned long id;
-			[[maybe_unused]] std::string   tyc;
-			[[maybe_unused]] unsigned long gaia;
-			[[maybe_unused]] unsigned long hyg;
-			[[maybe_unused]] unsigned long hip;
-			[[maybe_unused]] unsigned long hd;
-			[[maybe_unused]] unsigned long hr;
-			[[maybe_unused]] std::string   gl;
-			[[maybe_unused]] std::string   bayer;
-			[[maybe_unused]] std::string   flam;
-			[[maybe_unused]] std::string   con;
-			[[maybe_unused]] std::string   proper;
-			[[maybe_unused]] double        ra;
-			[[maybe_unused]] double        dec;
-			[[maybe_unused]] std::string   pos_src;
-			[[maybe_unused]] double        dist;
-			[[maybe_unused]] double        x0;
-			[[maybe_unused]] double        y0;
-			[[maybe_unused]] double        z0;
-			[[maybe_unused]] std::string   dist_src;
-			[[maybe_unused]] double        mag;
-			[[maybe_unused]] double        absmag;
-			[[maybe_unused]] double        ci;
-			[[maybe_unused]] std::string   mag_src;
-			[[maybe_unused]] double        rv;
-			[[maybe_unused]] std::string   rv_src;
-			[[maybe_unused]] double        pm_ra;
-			[[maybe_unused]] double        pm_dec;
-			[[maybe_unused]] double        pm_src;
-			[[maybe_unused]] double        vx;
-			[[maybe_unused]] double        vy;
-			[[maybe_unused]] double        vz;
-			[[maybe_unused]] double        spect;
-			[[maybe_unused]] std::string   spect_src;
-	    
-            [[maybe_unused]] explicit V3(const std::array<std::string, 34>& _values);
+			  unsigned long id;
+			  std::string   tyc;
+			  unsigned long gaia;
+			  unsigned long hyg;
+			  unsigned long hip;
+			  unsigned long hd;
+			  unsigned long hr;
+			  std::string   gl;
+			  std::string   bayer;
+			  std::string   flam;
+			  std::string   con;
+			  std::string   proper;
+			  double        ra;
+			  double        dec;
+			  std::string   pos_src;
+			  double        dist;
+			  double        x0;
+			  double        y0;
+			  double        z0;
+			  std::string   dist_src;
+			  double        mag;
+			  double        absmag;
+			  double        ci;
+			  std::string   mag_src;
+			  double        rv;
+			  std::string   rv_src;
+			  double        pm_ra;
+			  double        pm_dec;
+			  double        pm_src;
+			  double        vx;
+			  double        vy;
+			  double        vz;
+			  double        spect;
+			  std::string   spect_src;
+			  
+			  explicit V3(const std::array<std::string, 34>& _values);
 		};
 	};
 	
